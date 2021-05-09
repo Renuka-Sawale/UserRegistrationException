6f164fd0d262a9a613b8d7e1ba7fa97295f10400
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-09 19:15:17 +0530

    Added welcome message

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,8 +1,30 @@
 package com.userregistration;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args){
-        System.out.println("Welcome to hello World");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Exception");
+    }
+    /*private static final String First_Name_Check="(^[A-Z]{1}[a-z]{2,}$)";
+    private static final String Last_Name_Check=("^[A-Z]{1}[a-z]{2,}");
+    public boolean firstNameTest(String firstName) throws UserRegistrationException {
+        boolean test= Pattern.compile(First_Name_Check).matcher(firstName).matches();
+        if (test){
+            System.out.println(firstName + " Is valid");
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Given input is not valid");
+        }
     }
 
+    public boolean lastNameTest(String lastName) throws UserRegistrationException {
+        boolean test= Pattern.compile(First_Name_Check).matcher(lastName).matches();
+        if (test){
+            System.out.println(lastName + " Is valid");
+            return true;
+        }
+        else throw new UserRegistrationException("Given input is not valid");
+    }*/
 }
---
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,44 @@
+package com.userregistration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    /*@Test
+    public void givenFirstNameWhenProperShouldReturnResult() throws UserRegistrationException {
+        UserRegistration userregistration = new UserRegistration();
+        boolean result = userregistration.firstNameTest("Renuka");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstNameWhenNotProperShouldThrowException() throws UserRegistrationException {
+        UserRegistration userregistration = new UserRegistration();
+        try {
+            boolean result = userregistration.firstNameTest("ren");
+            Assertions.assertTrue(result);
+        }catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void givenLastNameWhenProperShouldReturnResult() throws UserRegistrationException {
+        UserRegistration userregistration = new UserRegistration();
+        boolean result = userregistration.lastNameTest("Sawale");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastNameWhenNotProperShouldThrowException() throws UserRegistrationException {
+        UserRegistration userregistration = new UserRegistration();
+        try {
+            boolean result = userregistration.lastNameTest("saw");
+            Assertions.assertTrue(result);
+        }catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+
+    }*/
+}
